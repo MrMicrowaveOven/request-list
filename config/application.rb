59f72c5dee5168315e6e1abc8bd0c170ed43625b
Benjamin <benj@zagorski.com>
2017-05-31 12:30:20 -0700

    Ran assets:precompile to all assets to load in heroku

--- config/application.rb
+++ config/application.rb
@@ -9,14 +9,14 @@ require "action_controller/railtie"
 require "action_mailer/railtie"
 require "action_view/railtie"
 require "action_cable/engine"
-# require "sprockets/railtie"
+require "sprockets/railtie"
 require "rails/test_unit/railtie"
 
 # Require the gems listed in Gemfile, including any gems
 # you've limited to :test, :development, or :production.
 Bundler.require(*Rails.groups)
 
-config.assets.initialize_on_precompile = false
+# config.assets.initialize_on_precompile = false
 module RequestList
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
