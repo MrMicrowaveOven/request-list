39ba2bf364d79600dfe304d15dc4b76fc4c6e004
Benjamin <benj@zagorski.com>
2017-05-31 12:10:44 -0700

    Undid initialize on precompile

--- config/application.rb
+++ config/application.rb
@@ -36,6 +36,6 @@ module RequestList
         resource '*', :headers => :any, :methods => [:get, :post, :options, :delete]
       end
     end
-    config.assets.initialize_on_precompile = false
+    # config.assets.initialize_on_precompile = false
   end
 end
