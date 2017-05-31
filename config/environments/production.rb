1baf9db4f4a689b062a952a64141026bbdefc830
Benjamin <benj@zagorski.com>
2017-05-31 12:38:55 -0700

    Moved compuressing assets to true

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -2,7 +2,7 @@ Rails.application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
 
 
-  # config.assets.compress = true
+  config.assets.compress = true
   # Code is not reloaded between requests.
   config.cache_classes = true
 
